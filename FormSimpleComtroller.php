4cec5d30cfb0e063840f835f3ac42c7a2ff6ca98
Dinh Phuong Anh <phuonganhdinh313@gmail.com>
2018-08-31 17:15:29 +0700

    myFile_form

---
+++ FormSimpleComtroller.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dtagvn
+ * Date: 8/31/18
+ * Time: 2:53 PM
+ */
+class FormSimpleComtroller
+{
+
+}
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,35 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/form", name="form_simple")
      */
-    public function indexAction(Request $request)
+    public function showAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        // creates a task and gives it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            // If you use PHP 5.3 or 5.4 you must use
+            // ->add('task', 'Symfony\Component\Form\Extension\Core\Type\TextType')
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+        return $this->render('/default/new.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 }
